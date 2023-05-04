0eb719c0fe67c9c6a9d1c73b90e066f3ebdfb144
Alexandra Miranda <alexyamiranda7@gmail.com>
2023-05-04 11:45:34 -0300

    feat: LoanSerializer and class Loan

--- loans/models.py
+++ loans/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Loan(models.Model):
+    loan_date = models.DateField(auto_now_add=True)
+    return_date = models.DateField()
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="users"
+    )
+    copy = models.ForeignKey(
+        "copies.Copy", on_delete=models.CASCADE, related_name="copies"
+    )
+
+    def __str__(self) -> str:
+        return f"{self.user} borrowed {self.copy}"
